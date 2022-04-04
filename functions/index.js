4f8db8753c9cddd02c3763376252cd97e5fe7a7b
CristiScarlat <78.cristi@gmail.com>
2022-04-04 22:16:24 +0300

    add cores

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+const {google} = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const calendar = google.calendar("v3");
+const functions = require("firebase-functions");
+const cors = require("cors");
+
+const googleCredentials = require("./credentials.json");
+
+const ERROR_RESPONSE = {
+  status: "500",
+  message: "There was an error adding an event to your Google calendar",
+};
+
+function getCalendars(auth) {
+  return new Promise(function(resolve, reject) {
+    calendar.calendarList.list({
+      auth: auth,
+    }, (error, res) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      }
+      console.log("Request successful", res);
+      resolve(res.data);
+    });
+  });
+}
+
+exports.listCalendars = functions.https.onRequest((req, res) => {
+  cors()(req, res, () => {
+    const oAuth2Client = new OAuth2(
+        googleCredentials.web.client_id,
+        googleCredentials.web.client_secret,
+        googleCredentials.web.redirect_uris[0]
+    );
+
+    oAuth2Client.setCredentials({
+      refresh_token: googleCredentials.refresh_token,
+    });
+
+    getCalendars(oAuth2Client)
+        .then((data) => {
+          console.log(data);
+          res.status(200).send(data);
+          return;
+        })
+        .catch((err) => {
+          console.error("Error adding event: " + err.message);
+          res.status(500).send(ERROR_RESPONSE);
+          return;
+        });
+  });
+});
+
